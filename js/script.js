58c4953d0c59624be27b8d1c1322e37788ccbd5d
diegomtzb <diegomtzb@hotmail.com>
2015-01-02 15:03:31 -0500

    ok

---
+++ js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+});
+
+//Mostrar mapa
+function showPosition() {
+    lat = "10.422026";
+    lon = "-75.533604";
+    latlon = new google.maps.LatLng(lat, lon);
+    mapholder = document.getElementById('mapholder');
+    //mapholder.style.height = '250px';
+    //mapholder.style.width = '500px';
+
+    var myOptions = {
+        center:latlon,zoom:14,
+        mapTypeId:google.maps.MapTypeId.ROADMAP,
+        mapTypeControl:false,
+        navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+    };
+
+    var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
+    var marker = new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
+}
+
+//Iniciar slick slider
+function slick_slider_init() {
+    $('.main-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+
+        onBeforeChange: function() {
+            var slideIndex = ($('.main-slider').slickCurrentSlide());
+            $('.captionimg' + slideIndex).hide( "fast", function() {
+                // Animation complete.
+            });
+        },
+        onAfterChange: function() {
+        //index of current slide
+        var slideIndex = ($('.main-slider').slickCurrentSlide());
+        $('.captionimg' + slideIndex).show( "slow", function() {
+            // Animation complete.
+        });
+
+
+        }
+    });
+}
+
+
